b72518ff5986fd3a2c6747e75f32f25719934515
Kumari Priya <priya@goel.pw>
2017-10-28 22:44:18 -0700

    Add click marker and show direction function in the google map

--- assets/js/googleMaps.js
+++ assets/js/googleMaps.js
@@ -1,3 +1,4 @@
+var start = {lat: 37.785, lng: -122.395};
 function initMap(items) {
     var myLatLng = {lat: 37.785, lng: -122.395};
     var Tname;
@@ -6,25 +7,77 @@ function initMap(items) {
     else{
       Tname = items;
     }
-
-
+  console.log(items)
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 11,
       center: myLatLng
     });
 
+  console.log(items);
   if(Tname.length>0){
     for (var j = 0; j < Tname.length; j++) {
-      // console.log(Tname[j]);
+      console.log(Tname[j]);
       var bizLatLang = {lat : Tname[j][2], lng: Tname[j][1]}
-      // console.log(bizLatLang);
+      console.log(bizLatLang);
+
       var marker = new google.maps.Marker({
         position: bizLatLang,
         map: map,
-        title: Tname[j][0]
+        title: Tname[j][0],
       });
+      // addMessage(marker, Tname[j][0]);
+      showDirection(marker);
     }
   }
-
-
 }
+
+  // function addMessage(marker, secretMessage) {
+  //     var infowindow = new google.maps.InfoWindow({
+  //       content: secretMessage
+  //     });
+  //
+  //     marker.addListener('click', function() {
+  //       infowindow.open(marker.get('map'), marker);
+  //       alert(marker.position);
+  //       initDirectionMap(start,marker.position,'DRIVING')
+  //     });
+  //
+  // }
+
+  function showDirection(marker){
+    marker.addListener('click', function() {
+      initDirectionMap(start,marker.position,'DRIVING')
+    });
+  }
+
+  // ==========================Directions =====================
+
+  function initDirectionMap(start,end,mode) {
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var directionsService = new google.maps.DirectionsService;
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 14,
+      center: start
+    });
+    directionsDisplay.setMap(map);
+
+    calculateAndDisplayRoute(directionsService,directionsDisplay,start,end,mode);
+    // document.getElementById('mode').addEventListener('change', function() {
+    //   calculateAndDisplayRoute(directionsService, directionsDisplay);
+    // });
+  }
+
+  function calculateAndDisplayRoute(directionsService, directionsDisplay,start,end,selectedMode) {
+    // var selectedMode = document.getElementById('mode').value;
+    directionsService.route({
+      origin: start,
+      destination: end,
+      travelMode: selectedMode
+    }, function(response, status) {
+      if (status == 'OK') {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
