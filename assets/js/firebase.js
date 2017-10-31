ff0ee8a8e686526b3c3a6de9d6b84e2ea99737dd
Kumari Priya <priya@goel.pw>
2017-10-30 22:18:08 -0700

    add/update favourite data in firebase.

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,74 +1,44 @@
-
-  // Initialize Firebase
-var config = {
-  apiKey: "AIzaSyC9CKyfiwZyZXVeQgQ-GoTufzljJuDYQjA",
-  authDomain: "codingbootcamp-6a8d8.firebaseapp.com",
-  databaseURL: "https://codingbootcamp-6a8d8.firebaseio.com",
-  projectId: "codingbootcamp-6a8d8",
-  storageBucket: "codingbootcamp-6a8d8.appspot.com",
-  messagingSenderId: "601031791714"
-};
+var fireBaseFav;
+// Initialize Firebase
+  var config = {
+      apiKey: "AIzaSyD4syou7Jr1SRGCxNvCcbjQXLugTPmGZUA",
+      authDomain: "rcp-game.firebaseapp.com",
+      databaseURL: "https://rcp-game.firebaseio.com",
+      projectId: "rcp-game",
+      storageBucket: "rcp-game.appspot.com",
+      messagingSenderId: "451138795484"
+    };
 
 firebase.initializeApp(config);
 
-const db = firebase.database();
-const deals = db.ref().child("deals");
-
 // Create a variable to reference the database.
+var database = firebase.database();
 
+// Get the data from firebase
+database.ref('/favs').once('value').then(function(snapshot){
+fireBaseFav = snapshot.val();
+  // console.log("firebase data");
+  // console.log(fireBaseFav);
+  });
+
+for (var key in fireBaseFav) {
+    if (fireBaseFav.hasOwnProperty(key)){
+      console.log("ItemID -" + key);
+      console.log("MerchantID - "+fireBaseFav[key].merchantId);
+      console.log("FavCount - "+fireBaseFav[key].favCount);
+}
+}
 
 $("#firstStar").on("click", function() {
-
   event.preventDefault();
-  
   deal = $(this).attr("data-did");
   merchant = $(this).attr("data-mid");
   count = 1;
 
-  deals.child(deal).set({
-    "merchantIdentifier": merchant,
-    "favoriteCount": count
-  });
-  // const rootRef = firebase.database().ref();
-
-  // const oneRef = rootRef.orderByChild('dealId').equalTo('4106722');
-  // console.log(oneRef);
-
-  // const events = db.child('codingbootcamp-6a8d8');
-  // const query = events
-  //                       .orderByChild('dealId')
-  //                       .equalTo('4106722')
-  //                       .limitToFirst(5)
-  
-  // query.on('value', snap => {
-  //   console.log(this);
-  // });
-
-  
-
-// const eventsRef = db.child('events');
-// const querydb = eventsRef.orderFunction().queryFunction();
-// const querydbten = eventsRef.orderByKey().limitToFirst(10);
-
-// console.log(querydb);
-// console.log(querydbten);
-
-// database.ref().once('value').then(function(snapshot){
-//   var snapArray = Object.values(snapshot.val());
-//   console.log(snapArray);
-//   });
-
-    //look for deal id in firebase
-    //if the deal is in the database then increment the favorite count by 1
-
-    //if the deal is NOT in the database then add the deal to the database
-
-    // database.ref().push({
-    //   merchantId: merchant,
-    //   dealId: deal, 
-    //   favoriteCount: count
-    // });
+// Insert/Update data in firebase
+    database.ref('favs/' + deal).set({
+      merchantId: merchant,
+      favCount: count
+    });
 
 });
-
-
